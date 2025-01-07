9e7b2bb846f180f6cec3ea386743303a2c811607
Kevin James <47490946+Kelvin285@users.noreply.github.com>
2025-01-07 11:38:14 -0500

    test

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
-const canvas = document.getElementById('fullPageCanvas');
-const context = canvas.getContext('2d');
+const { Application } = require("pixi.js");
 
-const ctx = canvas.getContext('2d');
-
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
-
-let time = 0;
-/// Update the canvas each frame
-function update() {
-    /// Clear the screen
-    ctx.fillStyle = '#FFFFFF';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = '#FF0000';
-    ctx.fillRect(Math.sin(time) * 50 + 300, 50, 200, 100);
-    time += 0.05;
-    requestAnimationFrame(update);
-}
-update();
+// Asynchronous IIFE
+(async () =>
+    {
+        // Create a PixiJS application.
+        const app = new Application();
+        
+        await app.init({background: '#000000', resizeTo: window});
+        
+        // Then adding the application's canvas to the DOM body.
+        document.body.appendChild(app.canvas);
+    })();
+    
